80203a73ee5e9232326c0a2a2b6c1abf73d8e01f
ram5353 <ramkiran5353@gmail.com>
2019-11-11 08:55:35 +0530

    first commit

---
+++ spring-cloud/limits-service/src/main/java/com/vwits/sdc/limitsservice/LimitsConfigurationController.java
@@ -0,0 +1,20 @@
+package com.vwits.sdc.limitsservice;
+
+import com.vwits.sdc.limitsservice.beaN.LimitConfiguration;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class LimitsConfigurationController {
+
+    @Autowired
+    Configuration configuration;
+
+    @GetMapping("/limits")
+    public LimitConfiguration retrieveLimitsFromConfiguration() {
+        return new LimitConfiguration(configuration.getMaximum(), configuration.getMinimum());
+
+    }
+}
---
+++ spring-cloud/limits-service/src/test/java/com/vwits/sdc/limitsservice/LimitsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vwits.sdc.limitsservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LimitsServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
